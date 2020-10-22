071ffa166ce42d6f8a4bfc76fa88da0e8a27a9cb
snowstormBG <snowstormsu@gmail.com>
2020-10-22 13:54:56 +0300

    Applied functionality for moving projects between active/finished projectsLists.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,91 @@
 //code here
+class DOMHelper {
+    static moveElement(elementId, newDestinationSelector) {
+        const element = document.getElementById(elementId);
+        const destinationEl = document.querySelector(newDestinationSelector);
+        destinationEl.append(element);
+    }
+
+    static clearEventListeners(element) {
+        const clonedElement = element.cloneNode(true);
+        element.replaceWith(clonedElement);
+        return clonedElement;
+    }
+}
+
+class Tooltip {
+
+}
+
+class ProjectItem {
+    #extraInfo;
+
+    constructor(id, updateProjectListsFunction) {
+        this.id = id;
+        this.updateProjectListsHandler = updateProjectListsFunction;
+        this.connectSwitchButton();
+        this.connectInfoButton();
+    }
+
+    connectInfoButton() {
+
+    }
+
+    update(updateProjectListsFn, type){
+        this.updateProjectListsHandler = updateProjectListsFn;
+        this.connectSwitchButton();
+    }
+
+    connectSwitchButton() {
+        const projectItemElement = document.getElementById(this.id);
+        let switchBtn = projectItemElement.querySelector('button:last-of-type');
+        switchBtn = DOMHelper.clearEventListeners(switchBtn);
+        switchBtn.addEventListener(
+            'click',
+            this.updateProjectListsHandler.bind(null, this.id)
+        );
+    }
+}
+
+class ProjectList {
+    projects = [];
+
+    constructor(type) {
+        this.type = type;
+        const projItems = document.querySelectorAll(`#${type}-projects ul > li`);
+        for (const projItem of projItems) {
+            this.projects.push(new ProjectItem(projItem.id, this.switchProject.bind(this)));
+        }
+
+
+    }
+
+    setSwitchHandlerFunction(switchHandlerFunction) {
+        this.switchHandler = switchHandlerFunction;
+    }
+
+    addProject(project) {
+        this.projects.push(project);
+        DOMHelper.moveElement(project.id, `#${this.type}-projects ul`);
+        project.update(this.switchProject.bind(this), this.type);
+    }
+
+    switchProject(projectID) {
+        // const projectIndex = this.#projects.findIndex(p => p.id === projectID);
+        // this.#projects.splice(projectIndex, 1);
+        this.switchHandler(this.projects.find(p => p.id === projectID));
+        this.projects = this.projects.filter(p => p.id !== projectID);
+    }
+
+}
+
+class App {
+    static init() {
+        const activeProjectsList = new ProjectList('active');
+        const finishedProjectsList = new ProjectList('finished');
+        activeProjectsList.setSwitchHandlerFunction(finishedProjectsList.addProject.bind(finishedProjectsList));
+        finishedProjectsList.setSwitchHandlerFunction(activeProjectsList.addProject.bind(activeProjectsList));
+    }
+}
+
+App.init();
